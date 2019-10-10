8156f57d4930a4298af363bb28996004a83c25ee
Cara Steffen <cara@treadstonedesign.com>
2019-10-10 16:46:23 -0600

    ready for deployment

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const cors = require("cors");
 const helmet = require("helmet");
 const POKEDEX = require("./pokedex.json");
 
-console.log(process.env.API_TOKEN);
-
 const app = express();
 
-app.use(morgan("dev"));
+const morganSetting = process.env.NODE_ENV === "production" ? "tiny" : "common";
+
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
@@ -39,8 +39,6 @@ app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get("Authorization");
 
-  console.log("validate bearer token middleware");
-
   //if (bearerToken !== apiToken) {
   //return res.status(401).json({ error: "Unauthorized request" });
   //}
@@ -58,8 +56,6 @@ function handleGetTypes(req, res) {
 }
 
 app.get("/types", handleGetTypes);
-
-//app.get("/pokemon", handleGetPokemon);
 
 app.get("/pokemon", function handleGetPokemon(req, res) {
   //res.send("Hello, Pokemon!");
@@ -83,8 +79,19 @@ app.get("/pokemon", function handleGetPokemon(req, res) {
   res.json(response);
 });
 
-const PORT = 8000;
+//4 parameters in middleware, express knows to treat this as error handler
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost: ${PORT}`);
+  //console.log(`Server listening at http://localhost: ${PORT}`);
 });
